f605c34330f6c321d1bdcdfd82e4caec916fe135
明洲 柳 <1226774890@qq.com>
2020-12-23 19:47:14 +0800

    修改权限，修改密码

---
+++ WindowsFormsApp2/changePower.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class changePower : Form
+    {
+        public changePower()
+        {
+            InitializeComponent();
+        }
+        //权限设置
+        private void button1_Click(object sender, EventArgs e)
+        {
+            login login = new login();
+            if(login.power != "管理员")
+            {
+                MessageBox.Show("您没有该权限");
+            }
+            else { 
+                databaseConnection db = new databaseConnection();
+                MySqlConnection conn = db.GetConnection();
+                string checkUser = "select power from manager where manager_id = '" + textBox1.Text + "';";
+                MySqlCommand check = new MySqlCommand(checkUser, conn);
+                MySqlDataReader reader = check.ExecuteReader();
+                if (reader.Read())
+                {
+                    reader.Close();
+                    string updataPower = "update manager set power = '管理员' where manager_id = '" + textBox1.Text + "';";
+                    MySqlCommand updata = new MySqlCommand(updataPower, conn);
+                    int res = updata.ExecuteNonQuery();
+                    if(res == 1)
+                    {
+                        MessageBox.Show("权限修改成功");
+                    }
+                    else
+                    {
+                        MessageBox.Show("失败");
+                    }
+                    updata.Dispose();
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp2/changepwd.cs
@@ -0,0 +1,65 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class changepwd : Form
+    {
+        public changepwd()
+        {
+            InitializeComponent();
+        }
+        //修改密码
+        private void button1_Click(object sender, EventArgs e)
+        {
+            login login = new login();
+            string manager_id = login.managerId;
+            databaseConnection db = new databaseConnection();
+            MySqlConnection conn = db.GetConnection();
+            string str = "select * from manager where manager_id = '" + manager_id + "';";
+            MySqlCommand cmd = new MySqlCommand(str, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                if (textBox1.Text != reader.GetString("password"))
+                {
+                    MessageBox.Show("密码错误");
+                    reader.Close();
+                }
+                else
+                {
+                    reader.Close();
+                    if (textBox2.Text == textBox3.Text)
+                    {
+                        string updataSql = "update manager set password ='" + textBox2.Text + "';";
+                        MySqlCommand changePwd = new MySqlCommand(updataSql, conn);
+                        int res = changePwd.ExecuteNonQuery();
+                        if (res == 1)
+                        {
+                            MessageBox.Show("密码修改成功");
+                        }
+                        changePwd.Dispose();
+                    }
+                    else
+                    {
+                        MessageBox.Show("二次确认密码失败，请重新输入");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("用户不存在");
+            }
+            reader.Close();
+            conn.Close();
+        }
+    }
+}
